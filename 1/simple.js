14c85f7f6c37d476700b11d00ee410fad1c8da34
arnold-anand <pagadalaarnold@karunya.edu.in>
2023-05-26 18:54:33 +0530

    simple completed

--- 1/simple.js
+++ 1/simple.js
@@ -1,3 +1,8 @@
 const colors = ["green", "red", "rgba(133,122,200)", "f15025"]
 let btn = document.getElementById("btn")
 let mycolor = document.getElementById("mycolor")
+btn.addEventListener("click", function(){
+    let randint = Math.floor(Math.random()*4)
+    document.getElementById('main').style.backgroundColor = colors[randint]
+    mycolor.innerText = colors[randint]
+})
